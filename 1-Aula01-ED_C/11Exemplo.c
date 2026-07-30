1980bd4b5466e42d733edd7c04d904c53cce8b10
Sergio Gustavo Medina Pereira <medinasergiogustavo@gmail.com>
2026-07-30 18:13:11 -0300

    Update 11Exemplo.c

--- 1-Aula01-ED_C/11Exemplo.c
+++ 1-Aula01-ED_C/11Exemplo.c
@@ -1,1 +1,13 @@
 
+#include <stdio.h> // Cabeçalho/Header: Diretiva para usar funções de entrada e saída 
+#include <stdlib.h>
+
+int main(void)
+{ // Ponto de partida obrigatório de todo programa 
+    printf("Bom dia, turma. \n");
+    printf("Hello World\n");
+
+    system("Pause"); //Somente para o windows
+    printf("Fim do programa\n");
+    return 0;
+}
